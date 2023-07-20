abcda321f9a99e92b2b375ce6a57635fcbade192
Vaidas Datenis <datenis.v@gmail.com>
2023-07-20 09:11:50 +0300

    Hide stuff.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,13 +1,15 @@
+import { keys } from "key-values";
+
 export const environment = {
   production: false,
   firebaseConfig: {
-    projectId: 'akcijoslt-8862e',
-    appId: '1:778114411173:web:d792746250af6f388bfb4c',
-    databaseURL: 'https://akcijoslt-8862e-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'akcijoslt-8862e.appspot.com',
-    apiKey: 'AIzaSyBG6ydtu6Q37C2HJJeaYnhBiq_Ex3GoT80',
-    authDomain: 'akcijoslt-8862e.firebaseapp.com',
-    messagingSenderId: '778114411173',
-    measurementId: 'G-MYR9QX18FQ',
+    projectId: keys.projectId,
+    appId: keys.appId,
+    databaseURL: keys.databaseURL,
+    storageBucket: keys.storageBucket,
+    apiKey: keys.apiKey,
+    authDomain: keys.authDomain,
+    messagingSenderId: keys.messagingSenderId,
+    measurementId: keys.measurementId,
   },
 };
